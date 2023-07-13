87aa9e664186a8c418c3e89bb26774197cf741e7
arunraghunath <arunattech@gmail.com>
2023-07-13 21:05:00 +0100

    New package controllers - Static Handler - common function of type http.HandlerFunc to execute the template. Parse all template files on server startup.

--- main.go
+++ main.go
@@ -2,38 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
+	"github.com/arunraghunath/sharepics/controllers"
 	"github.com/arunraghunath/sharepics/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
-
-func processTemplate(w http.ResponseWriter, htmlfile string) {
-	ts, err := views.Parse(htmlfile)
-	if err != nil {
-		log.Print(err)
-		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
-		return
-	}
-	ts.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	file := "./templates/home.html"
-	processTemplate(w, file)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	file := "./templates/contact.html"
-	processTemplate(w, file)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	file := "./templates/faq.html"
-	processTemplate(w, file)
-}
 
 func urlParamHandler(w http.ResponseWriter, r *http.Request) {
 	val := chi.URLParam(r, "param")
@@ -47,9 +22,25 @@ func urlParamHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
-	router.Get("/", homeHandler)
-	router.Get("/contact", contactHandler)
-	router.Get("/faq", faqHandler)
+
+	tpl, err := views.Parse("./templates/home.html")
+	if err != nil {
+		panic(err)
+	}
+	router.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse("./templates/contact.html")
+	if err != nil {
+		panic(err)
+	}
+	router.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse("./templates/faq.html")
+	if err != nil {
+		panic(err)
+	}
+	router.Get("/faq", controllers.StaticHandler(tpl))
+
 	router.Get("/testurlparam", urlParamHandler)
 
 	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
