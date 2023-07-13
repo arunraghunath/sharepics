e0bb6d1b07729565b4b5d06784084ac03ee569a9
arunraghunath <arunattech@gmail.com>
2023-07-13 12:24:41 +0100

    3rd party router - Chi router with Logger middleware

--- main.go
+++ main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-type Router struct {
-}
-
-func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	pathHandler(w, r)
-}
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to SHarepics website!!</h1>")
+	fmt.Fprintf(w, "<h1>Welcome to Sharepics website!!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,32 +22,27 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>FAQ Page</h1><p>All help you need can be found here</p>"))
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	/*if r.URL.Path == "/" {
-		homeHandler(w, r)
-	} else if r.URL.Path == "/contact" {
-		contactHandler(w, r)
-	} else {
-		//w.WriteHeader(404)
-		//w.Write([]byte("Page not found"))
-		http.Error(w, "Page not found", 404)
-	}*/
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", 404)
+func urlParamHandler(w http.ResponseWriter, r *http.Request) {
+	val := chi.URLParam(r, "param")
+	if val == "" {
+		fmt.Fprintf(w, "Empty param")
+		return
 	}
+	fmt.Fprintf(w, "Param value is %s", val)
 }
 
 func main() {
-	//http.ListenAndServe(":2020", &Router{})
-	//http.ListenAndServe(":2020", http.HandlerFunc(pathHandler))
-	var router http.HandlerFunc
-	router = pathHandler
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", homeHandler)
+	router.Get("/contact", contactHandler)
+	router.Get("/faq", faqHandler)
+	router.Get("/testurlparam", urlParamHandler)
+
+	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", 404)
+	})
+
+	fmt.Println("Starting the server at :2020")
 	http.ListenAndServe(":2020", router)
 }
